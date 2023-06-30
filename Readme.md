e5c5bcda19890727fa37b92e806ed59a53cbf9df
Emre <ct.edag@gmail.com>
2023-06-30 07:53:44 +0300

    Exercise11

--- Readme.md
+++ Readme.md
@@ -2,3 +2,11 @@ acp = "!f() { git add . && git commit -m \"$@\" && git push; }; f"
 
 
 git acp "Commit Message"
+
+
+
+GET http://localhost:3000/api/planets to retrieve all planets.
+GET http://localhost:3000/api/planets/1 to retrieve a planet by ID.
+POST http://localhost:3000/api/planets with JSON body {"name": "Venus"} to create a new planet.
+PUT http://localhost:3000/api/planets/1 with JSON body {"name": "New Earth"} to update an existing planet.
+DELETE http://localhost:3000/api/planets/1 to delete a planet by ID.
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
+import Joi from 'joi';
 
 dotenv.config();
 
@@ -12,34 +13,78 @@ app.use(morgan('dev'));
 
 // Dummy Database
 interface Planet {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 
-const planets: Planet[] = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
+let planets: Planet[] = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
 ];
 
+// Validation Schema
+const planetSchema = Joi.object({
+    name: Joi.string().min(3).required()
+});
+
 // Routes
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express with TypeScript!');
+app.get('/api/planets', (req: Request, res: Response) => {
+    res.status(200).json(planets);
+});
+
+app.get('/api/planets/:id', (req: Request, res: Response) => {
+    const planet = planets.find(p => p.id === parseInt(req.params.id));
+    if (!planet) return res.status(404).send('Planet not found');
+    res.status(200).json(planet);
+});
+
+app.post('/api/planets', (req: Request, res: Response) => {
+    const { error } = planetSchema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const planet: Planet = {
+        id: planets.length + 1,
+        name: req.body.name
+    };
+    planets.push(planet);
+    res.status(201).json({ msg: 'Planet created successfully' });
+});
+
+app.put('/api/planets/:id', (req: Request, res: Response) => {
+    const planet = planets.find(p => p.id === parseInt(req.params.id));
+    if (!planet) return res.status(404).send('Planet not found');
+
+    const { error } = planetSchema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    planet.name = req.body.name;
+    res.status(200).json({ msg: 'Planet updated successfully' });
+});
+
+app.delete('/api/planets/:id', (req: Request, res: Response) => {
+    const index = planets.findIndex(p => p.id === parseInt(req.params.id));
+    if (index === -1) return res.status(404).send('Planet not found');
+
+    planets.splice(index, 1);
+    res.status(200).json({ msg: 'Planet deleted successfully' });
 });
 
 // Error Handling Middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 // Start the Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
+
+
